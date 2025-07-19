89b000f290b40e0f17637c226e538de8fadb1d9f
CoderShivam <shivamkumargupta3456@gmail.com>
2025-07-19 18:13:07 +0530

    Update PieChart.jsx

--- client/src/components/PieChart.jsx
+++ client/src/components/PieChart.jsx
@@ -16,7 +16,7 @@ const PieChart = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:7643/phq9/result/${userId}`);
+        const response = await axios.get(`https://serenditymind-backend.onrender.com/phq9/result/${userId}`);
         const data = response.data.data;
 
         // Group data by severity
